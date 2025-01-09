04895fa1b1e6fc19e4f482df3ed1131f08d641dc
Leandro Schwegler <leandro_schwegler@sluz.ch>
2025-01-09 08:33:53 +0100

    UnitTests

---
+++ src/test/BetTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Projekt_M320.test;
+
+
+[TestFixture]
+public class BetTests
+{
+    [Test]
+    public void NumberBet_Evaluate_ShouldReturnTrue_WhenNumberMatches()
+    {
+        var bet = new NumberBet(100, 5);
+        var result = bet.Evaluate(5);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void NumberBet_Evaluate_ShouldReturnFalse_WhenNumberDoesNotMatch()
+    {
+        var bet = new NumberBet(100, 5);
+        var result = bet.Evaluate(3);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void ColorBet_Evaluate_ShouldReturnTrue_WhenColorMatches()
+    {
+        var bet = new ColorBet(100, "Red");
+        var result = bet.Evaluate(3); // 3 is odd, so Red
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void ColorBet_Evaluate_ShouldReturnFalse_WhenColorDoesNotMatch()
+    {
+        var bet = new ColorBet(100, "Black");
+        var result = bet.Evaluate(3); // 3 is odd, so Red
+        Assert.IsFalse(result);
+    }
+}
